3e9e3b7be81ebf4f3d9e728e83cde4cb568ce4a2
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-06-16 23:37:33 -0400

    Add form submission js

---
+++ src/js/components/form.js
@@ -0,0 +1,96 @@
+import application from '../app';
+
+application.component('.js-form', {
+  init() {
+    this.form = this.node.querySelector('form');
+    this.inputs = this.form.querySelectorAll('input,textarea');
+    this.submitBtn = this.form.querySelector('button[type="submit"]');
+
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.hideError();
+      this.startLoading();
+      this.submitData(this.readData()).then( () => {
+        this.node.classList.add("is-submitted");
+      }).catch((e) => {
+        console.error(e);
+        this.stopLoading();
+        this.showError();
+      });
+    });
+
+    this.form.addEventListener('input', (e) => {
+      this.invalidate();
+    });
+
+    this.invalidate();
+  },
+
+  submitData(data) {
+    console.log("Submitted:", data);
+    return new Promise((resolve, reject) => {
+      const delay = (Math.random() * 3000) | 0;
+
+      if (data.email === 'fail') {
+        setTimeout(reject, delay);
+      } else {
+        setTimeout(resolve, delay);
+      }
+    });
+  },
+
+  readData() {
+    let data = {};
+    for(let input of this.inputs) {
+      data[input.name] = input.value;
+    }
+    return data;
+  },
+
+  startLoading(){
+    this.prevSubmitText = this.submitBtn.textContent;
+    this.submitBtn.disabled = true;
+    this.submitBtn.textContent = ".";
+    let counter = 1;
+    this.loadingTid = setInterval(() => {
+      counter++;
+      if(counter > 5) counter = 1;
+      this.submitBtn.textContent = ".".repeat(counter);
+    },
+    200);
+  },
+
+  stopLoading(){
+    if (this.loadingTid) clearInterval(this.loadingTid);
+    this.submitBtn.textContent = this.prevSubmitText;
+    this.submitBtn.disabled = false;
+  },
+
+  showError(){
+    this.node.classList.add('is-error');
+  },
+
+  hideError() {
+    this.node.classList.remove('is-error');
+  },
+
+  invalidate() {
+    let valid = true;
+
+    this.hideError();
+
+    for(let input of this.inputs) {
+      if (input.required && !input.value) {
+        valid = false;
+      }
+    }
+
+    if (valid) {
+      this.submitBtn.disabled = false;
+    } else {
+      this.submitBtn.disabled = true;
+    }
+
+    return valid;
+  }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import './utils/fonts-loaded';
 import './components/scroll_link';
 import './components/bubble';
 import './components/animation-controller';
+import './components/form';
