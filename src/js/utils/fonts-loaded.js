3496d180d26931799d94ce085980a76c7b2b09e8
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-06-18 18:55:54 -0400

    use Stem for headings

--- src/js/utils/fonts-loaded.js
+++ src/js/utils/fonts-loaded.js
@@ -1,11 +1,16 @@
 import FontFaceObserver from 'fontfaceobserver';
 
-var fontNormal = new FontFaceObserver('Stem Text');
-var fontBold = new FontFaceObserver('Stem Text', {
+var stemTextNormal = new FontFaceObserver('Stem Text');
+var stemTextBold = new FontFaceObserver('Stem Text', {
+  weight: 700
+});
+var stemNormal = new FontFaceObserver('Stem');
+var stemBold = new FontFaceObserver('Stem', {
   weight: 700
 });
 
-Promise.all([fontNormal.load(), fontBold.load()]).then(function () {
+
+Promise.all([stemTextNormal.load(), stemTextBold.load(), stemNormal.load(), stemBold.load()]).then(function () {
   document.documentElement.classList.add('fonts-loaded');
 
   // Optimization for Repeat Views
