7353afc1cc03a64a3e5e94f93058bc6c685f20bf
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-06-08 19:11:17 +0300

    Add links to Docs

--- src/js/components/bubble.js
+++ src/js/components/bubble.js
@@ -4,6 +4,11 @@ import nodeY from '../utils/node-y';
 application.component('.js-bubble', {
   init() {
     this.delta = this.node.dataset['delta'] | 0;
+    this.invertAnchors = (this.node.dataset['invertAnchors'] || '')
+      .split(',')
+      .map((id) => document.getElementById(id))
+      .filter( (el) => el)
+    this.inverted = false;
     this.eventHandler = this.eventHandler.bind(this);
     this.eventHandler();
     this.watch();
@@ -12,12 +17,14 @@ application.component('.js-bubble', {
   eventHandler() {
     const y = nodeY(this.node);
     const viewTop = document.body.scrollTop || document.documentElement.scrollTop;
-      
+
     if (viewTop > y + this.delta) {
       this.bubble();
     } else {
       this.unbubble();
     }
+
+    this.maybeInvert(viewTop + this.delta + (this.node.offsetHeight / 2));
   },
 
   watch() {
@@ -36,6 +43,30 @@ application.component('.js-bubble', {
     if (this.bubbling) {
       this.bubbling = false;
       this.node.classList.remove('is-bubbling');
+    }
+  },
+
+  maybeInvert(y) {
+    if (!this.invertAnchors.length) return;
+
+    let invert = false;
+
+    for (let el of this.invertAnchors) {
+      const y0 = nodeY(el)
+      const y1 = y0 + el.offsetHeight
+      if (y0 <= y && y1 > y) {
+        invert = true;
+        break;
+      }
+    }
+
+    if (this.inverted === invert) return;
+
+    this.inverted = invert;
+    if (this.inverted) {
+      this.node.classList.add('is-inverted')
+    } else {
+      this.node.classList.remove('is-inverted')
     }
   }
 });
