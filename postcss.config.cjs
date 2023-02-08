2c504f84fbccaba558a11d72e1426c92e6d851cc
Vladimir Dementyev <palkan@hey.com>
2023-02-08 18:35:36 -0500

    Migrate to Vite

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    "postcss-preset-env": {browsers: 'last 2 versions'},
+    "postcss-nested": {},
+    "postcss-custom-media": {},
+    "postcss-easings": {},
+  },
+};
