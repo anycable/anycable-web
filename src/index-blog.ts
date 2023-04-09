826ee4e1e0527dad778a9cd9f7560234dd12f3cb
Rodion Chernikov <r.chernikov@vk.team>
2023-04-09 23:27:25 +0300

    ref: refactoring

--- src/index-blog.ts
+++ src/index-blog.ts
@@ -3,10 +3,7 @@ import './js/components/gtm-footer-link';
 import './js/components/gtm-form';
 import './js/components/header-show-hide';
 import './js/components/popups-show-hide';
-import './js/components/learn-more-scroll';
-import './js/components/slide-show';
-import './js/components/carousel';
-import './index.scss';
+import './index-blog.scss';
 
 //to prevent flickering on load
 window.addEventListener('load', () => {
--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import './components/gtm-link';
-import './components/gtm-footer-link';
-import './components/gtm-form';
-import './components/learn-more-scroll';
-import './components/header-show-hide';
-import './components/popups-show-hide';
