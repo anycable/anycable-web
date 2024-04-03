fcf2fd86b07d0399a5b7890e8ab6f5abd2aad2aa
Vladimir Dementyev <palkan@hey.com>
2024-04-03 11:39:44 -0700

    + code snippets tabs

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import './js/components/header-show-hide';
 import './js/components/popups-show-hide';
 import './js/components/learn-more-scroll';
 import './js/components/demo';
+import './js/components/tabs';
 import './index.scss';
