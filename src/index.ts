c44b9076ccb2edb564068676258424a35e15504d
Vladimir Dementyev <palkan@hey.com>
2024-04-09 17:14:19 -0700

    Activate cursors only on desktop

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import './js/components/popups-show-hide';
 import './js/components/learn-more-scroll';
 import './js/components/demo';
 import './js/components/tabs';
+import './js/components/cursors';
 import './index.scss';
-
-import 'anycable-elements/cursors-element';
